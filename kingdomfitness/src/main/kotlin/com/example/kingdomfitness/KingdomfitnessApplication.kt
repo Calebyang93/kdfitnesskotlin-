861005473609922d37132d4b947079be0f2d9a69
caleb.yanghannchih93@gmail.com <caleb.yanghannchih93@gmail.com>
2025-09-04 10:51:38 +0800

    Boilerplate code for kotlin java springboot project for kingdomfitnesswebaplication on shopify.

---
+++ kingdomfitness/src/main/kotlin/com/example/kingdomfitness/KingdomfitnessApplication.kt
@@ -0,0 +1,11 @@
+package com.example.kingdomfitness
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KingdomfitnessApplication
+
+fun main(args: Array<String>) {
+	runApplication<KingdomfitnessApplication>(*args)
+}
